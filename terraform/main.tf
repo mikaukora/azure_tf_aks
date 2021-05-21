5c490c10c8858c2b781c22e9ab1bfde415e83131
Mika Kaukoranta <mika.kaukoranta@mavericks.fi>
2021-05-21 16:08:22 +0300

    Add actions and terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tstate"
+    storage_account_name = "mkstorage1621600706"
+    container_name       = "tfstatedevops"
+    key                  = "tfstatedevops.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+#Create Resource Group
+resource "azurerm_resource_group" "mkops" {
+  name     = "mkops"
+  location = "northeurope"
+}
+
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "mkops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "northeurope"
+  resource_group_name = azurerm_resource_group.mkops.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.mkops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
+  
+
