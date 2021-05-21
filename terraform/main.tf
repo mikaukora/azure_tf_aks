23e1a2a9e29f645d6edc2ef3e0470acc4630c648
Mika Kaukoranta <mika.kaukoranta@mavericks.fi>
2021-05-21 17:01:20 +0300

    Fix resource group name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tstate"
+    resource_group_name  = "tfstate"
     storage_account_name = "mkstorage1621600706"
     container_name       = "tfstatedevops"
     key                  = "tfstatedevops.tfstate"
